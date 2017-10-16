18dae78fe598a1d17326702753db6870d7be8efa
kirstenmyette <kirstenmyette@aol.com>
2017-10-16 11:51:21 -0500

    incomplete lab unable to create div on button click

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var shapecontainer;
+var value = document.getElementById("value");
 document.addEventListener('DOMContentLoaded', function () {
-    shapecontainer = document.getElementById('shapecontianer');
+    shapecontainer = document.getElementById('shapecontainer');
+    document.body.appendChild(shapecontainer);
     var Shape = /** @class */ (function () {
         function Shape() {
             var _this = this;
+            this.height = document.getElementById("height");
+            this.width = document.getElementById("width");
+            this.radius = document.getElementById("radius");
             this.div = document.createElement('div');
             this.div.addEventListener('click', function () {
                 document.createElement('div');
@@ -27,29 +32,42 @@ document.addEventListener('DOMContentLoaded', function () {
     }());
     var square = /** @class */ (function (_super) {
         __extends(square, _super);
-        function square() {
-            return _super !== null && _super.apply(this, arguments) || this;
+        function square(height) {
+            var _this = _super.call(this) || this;
+            _this.div.style.height = document.getElementById("height").value;
+            shapecontainer.appendChild(_this.div);
+            return _this;
         }
         return square;
     }(Shape));
     var rectangle = /** @class */ (function (_super) {
         __extends(rectangle, _super);
-        function rectangle() {
-            return _super !== null && _super.apply(this, arguments) || this;
+        function rectangle(height, width) {
+            var _this = _super.call(this) || this;
+            _this.div.style.height = document.getElementById("height").value;
+            _this.div.style.width = document.getElementById("width").value;
+            shapecontainer.appendChild(_this.div);
+            return _this;
         }
         return rectangle;
     }(Shape));
     var circle = /** @class */ (function (_super) {
         __extends(circle, _super);
-        function circle() {
-            return _super !== null && _super.apply(this, arguments) || this;
+        function circle(radius) {
+            var _this = _super.call(this) || this;
+            _this.div.style.borderRadius = document.getElementById("radius").value;
+            shapecontainer.appendChild(_this.div);
+            return _this;
         }
         return circle;
     }(Shape));
     var triangle = /** @class */ (function (_super) {
         __extends(triangle, _super);
-        function triangle() {
-            return _super !== null && _super.apply(this, arguments) || this;
+        function triangle(height) {
+            var _this = _super.call(this) || this;
+            _this.div.style.height = document.getElementById("height").value;
+            shapecontainer.appendChild(_this.div);
+            return _this;
         }
         return triangle;
     }(Shape));
--- script.ts
+++ script.ts
@@ -1,22 +1,31 @@
 let shapecontainer: HTMLElement;
+let value: HTMLInputElement = (<HTMLInputElement>document.getElementById("value"));
 
 document.addEventListener('DOMContentLoaded', () => {
-    shapecontainer = document.getElementById('shapecontianer');
+    shapecontainer = document.getElementById('shapecontainer');
+    document.body.appendChild(shapecontainer);
 
-interface IShape{
-    value: any;
-    div:HTMLDivElement;
-}
+    interface IShape {
+        div: HTMLDivElement;
+        value: number;
+        height: any;
+        width: any;
+        radius: any;
+       
+    }
 
 
     class Shape implements IShape {
-        div:HTMLDivElement;
-        value: any;
+        div: HTMLDivElement;
+        value: number;
+        height: any = (<HTMLInputElement>document.getElementById("height"));
+        width: any = (<HTMLInputElement>document.getElementById("width"));
+        radius: any = (<HTMLInputElement>document.getElementById("radius"));
 
-        constructor(){
+        constructor() {
             this.div = document.createElement('div');
             this.div.addEventListener('click', () => {
-                document.createElement('div');
+            document.createElement('div');
             });
             this.div.addEventListener('dblclick', () => {
                 this.div.remove();
@@ -25,23 +34,40 @@ interface IShape{
         }
     }
 
-    class square extends Shape  {
-       
+    class square extends Shape {
+        constructor(height) {
+            super();
+            this.div.style.height = (<HTMLInputElement>document.getElementById("height")).value;
+            shapecontainer.appendChild(this.div);
+            
+        }
     }
 
     class rectangle extends Shape {
-        
+        constructor(height, width) {
+            super();
+            this.div.style.height = (<HTMLInputElement>document.getElementById("height")).value;
+            this.div.style.width = (<HTMLInputElement>document.getElementById("width")).value;
+            shapecontainer.appendChild(this.div);
+
+        }
     }
 
     class circle extends Shape {
-        
+        constructor(radius) {
+            super();
+            this.div.style.borderRadius = (<HTMLInputElement>document.getElementById("radius")).value;
+            shapecontainer.appendChild(this.div);
+        }
     }
 
     class triangle extends Shape {
-        
+        constructor(height) {
+            super();
+            this.div.style.height = (<HTMLInputElement>document.getElementById("height")).value;
+            shapecontainer.appendChild(this.div);
+        }
     }
 
 
-
-
-});
+    });
